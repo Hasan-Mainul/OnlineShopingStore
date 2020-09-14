d0baaf319f5e1c3f80cbe473790f7afaa48b70ba
mainul hasan <mainulhasan0112358@gmail.com>
2020-09-14 22:51:41 +0600

    Add project files.

---
+++ OnlineShopingStore/Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using OnlineShopingStore.DAL;
+using OnlineShopingStore.Models;
+using OnlineShopingStore.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShopingStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+
+        // GET: Admin
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Categories()
+        {
+            List<Tbl_Category> allcategories = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecordsIQueryable().Where(i => i.IsDelete == false).ToList();
+            return View(allcategories);
+        }
+
+        public ActionResult AddCategory()
+        {
+            return UpdateCategory(0);
+        }
+
+        public ActionResult UpdateCategory(int categoryId)
+        {
+            CategoryDetail cd;
+            if (categoryId != null)
+            {
+                cd = JsonConvert.DeserializeObject<CategoryDetail>(JsonConvert.SerializeObject(_unitOfWork.GetRepositoryInstance<Tbl_Category>().GetFirstorDefault(categoryId)));
+            }
+            else
+            {
+                cd = new CategoryDetail();
+            }
+            return View("UpdateCategory", cd);
+
+        }
+
+        public ActionResult Product()
+        {
+            return View(_unitOfWork.GetRepositoryInstance<Tbl_Product>().GetProduct());
+        }
+
+    }
+}
---
+++ OnlineShopingStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineShopingStore.MvcApplication" Language="C#" %>
